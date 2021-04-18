708b88a957c8de1d67967a49ee7735b03092e911
Jose Antonio Olivari <antonio.olivari@gmail.com>
2021-04-18 11:38:47 +0200

    el buscador ya funciona

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-let pokemon = "prueba"
-console.log(pokemon)
+
+let submitButton1 = document.getElementById('sbutton')
+submitButton1.addEventListener('click', event => {
+  event.preventDefault()
+  let search = document.getElementById("search")
+  let sabueso = {
+    pokemonName:search.value
+  }
+  console.log(sabueso)
+    })
+
+    
+
+
+
+
+/* buscar pokemon usando la ap Les dejo las referencias a PokeAPI, a POSTMAN y al repositorio que hicimos ayer (auth-ui).
+Documentación https://pokeapi.co/docs/v2
+Página de PokeAPI con una herramienta de ejemplo https://pokeapi.co/
+Página de POSTMAN https://www.postman.com/
+Repositorio de auth-ui https://github.com/linoman/auth-ui */
+
+
